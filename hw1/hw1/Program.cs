24dabc61261c7de06cda1a6bca3cd9acd7fb41c7
Decorus2009 <Decorus2009@mail.ru>
2017-10-05 02:21:07 +0500

    Refactored and bugs fixed

--- hw1/hw1/Program.cs
+++ hw1/hw1/Program.cs
@@ -1,33 +1,9 @@
-﻿using static System.Console;
-using System.Collections.Generic;
-
-namespace hw1
+﻿namespace hw1
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            // some simple tests
-            var trie = new Trie();
-            var words = new List<string> {"Hello", "Hello, World!", "C#", "Java", "C++", "i", "in", "inn", "inner"};
-            foreach (var word in words)
-            {
-                WriteLine($"Adding \"{word}\": {trie.Add(word)}");
-                WriteLine($"Contains \"{word}\"?: {trie.Contains("Hello")}\n");                
-            }
-
-            
-            var removeList = new List<string> {"inn", "in", "inner", "i"};
-            foreach (var removeWord in removeList)
-            {
-                WriteLine($"Removing \"{removeWord}\": {trie.Remove(removeWord)}");   
-                WriteLine($"Trie size: {trie.Size()}");
-            }
-            
-            foreach (var removeWord in removeList)
-            {
-                WriteLine($"Contains \"{removeWord}\"?: {trie.Contains(removeWord)}\n");                
-            }
         }
     }
 }
--- hw1/hw1/Trie.cs
+++ hw1/hw1/Trie.cs
@@ -1,53 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace hw1
 {
-    public class Trie
+    public class Trie : ITrie
     {
         private class TrieNode
         {
-            private readonly Dictionary<char, TrieNode> _children = new Dictionary<char, TrieNode>();
+            private readonly Dictionary<char, TrieNode> _children;
 
-            internal TrieNode Parent { get; private set; }
-            internal bool IsTerminal { get; set; }
-            internal int TraversalNumber { get; set; }
-
-            internal bool HasChildBy(char c) => _children.ContainsKey(c);
-
-            internal void AddChildBy(char c) => _children[c] = new TrieNode {Parent = this};
-
-            internal bool HasChildren() => _children.Count != 0;
-
-            internal TrieNode NextChildBy(char c) => _children[c];
-
-            // Should I make deleted nodes null to help GC? There're no more references to each deleted node
-            public void RemoveChildBy(char c)
+            public TrieNode()
             {
-                var child = _children[c];
-                // remove node
-                child = null;
-                // remove edge
-                _children.Remove(c);
+                _children = new Dictionary<char, TrieNode>();
             }
-//             public void RemoveChildBy(char c) => _children.Remove(c);
+
+            public TrieNode Parent { get; private set; }
+            public bool IsTerminal { get; set; }
+            public int TraversalNumber { get; set; }
+
+            public bool HasChildBy(char c) => _children.ContainsKey(c);
+
+            public void AddChildBy(char c) => _children[c] = new TrieNode {Parent = this};
+
+            public bool HasChildren() => _children.Any();
+
+            public TrieNode NextChildBy(char c) => _children[c];
+
+            public void RemoveChildBy(char c) => _children.Remove(c);
         }
 
-        private class TrieNodeException : Exception
-        {
-        }
         private readonly TrieNode _root = new TrieNode();
         private int _size;
 
-        /// Expected complexity: O(|element|)
-        /// Returns true if this set did not already contain the specified element
+
         public bool Add(string element)
         {
             if (Contains(element))
             {
                 return false;
             }
-
+            
             var curNode = _root;
             foreach (var c in element)
             {
@@ -60,83 +53,52 @@ namespace hw1
                 curNode.TraversalNumber++;
             }
             curNode.IsTerminal = true;
-            _size++;
+            ++_size;
             return true;
         }
 
-        /// Expected complexity: O(|element|)
-        public bool Contains(string element)
-        {
-            try
-            {
-                // last found node must be terminal (otherwise it might be some intermediate node in another word)
-                return FindLastNodeFor(element).IsTerminal;
-            }
-            catch (TrieNodeException e)
-            {
-                return false;
-            }
-        }
+        // last found node must be terminal (otherwise it might be some intermediate node in another word)
+        public bool Contains(string element) => FindLastNodeFor(element)?.IsTerminal ?? false;
 
-        /// Expected complexity: O(|element|)
-        /// Returns true if this set contained the specified element
         public bool Remove(string element)
         {
-            try
-            {
-                var curNode = FindLastNodeFor(element);
-                if (!curNode.IsTerminal)
-                {
-                    return false;
-                }
-                // in the terminal node now...
-
-                curNode.IsTerminal = false;
-                if (!curNode.HasChildren())
-                {
-                    // remove nodes until: 
-                    //     another terminal node of another shorter word is found, 
-                    //     the root node is reached.
-                    for (var i = element.Length - 1; i >= 0 && !curNode.IsTerminal; i--)
-                    {
-                        var c = element[i];
-                        curNode = curNode.Parent;
-                        curNode.RemoveChildBy(c);
-                    }
-                }
-                // decrease traversal number for the remaining nodes
-                while (curNode != _root)
-                {
-                    curNode.TraversalNumber--;
-                    curNode = curNode.Parent;
-                }
-                _size--;
-
-                return true;
-            }
-            catch (TrieNodeException e)
+            var curNode = FindLastNodeFor(element);
+            if (curNode == null || !curNode.IsTerminal)
             {
                 return false;
             }
+            // in the terminal node now...
+
+            curNode.IsTerminal = false;
+            if (!curNode.HasChildren())
+            {
+                // remove nodes until: 
+                //     another terminal node of another shorter word is found, 
+                //     the root node is reached.
+                foreach (var c in element.Reverse())
+                {
+                    if (curNode.IsTerminal)
+                    {
+                        break;
+                    }
+                    curNode = curNode.Parent;
+                    curNode.RemoveChildBy(c);
+                }
+            }
+            // decrease traversal number for remaining nodes
+            while (curNode != _root)
+            {
+                curNode.TraversalNumber--;
+                curNode = curNode.Parent;
+            }
+            --_size;
+            return true;
         }
 
-        /// Expected complexity: O(1)
         public int Size() => _size;
 
-        /// Expected complexity: O(|prefix|)
-        /// suppose that prefix is contained in the Trie
-        public int HowManyStartsWithPrefix(string prefix)
-        {
-            try
-            {
-                return FindLastNodeFor(prefix).TraversalNumber;
-            }
-            catch (TrieNodeException e)
-            {
-                return 0;
-            }
-        }
-        
+        public int HowManyStartsWithPrefix(string prefix) => FindLastNodeFor(prefix)?.TraversalNumber ?? 0;
+
 
         private TrieNode FindLastNodeFor(string element)
         {
@@ -145,7 +107,7 @@ namespace hw1
             {
                 if (!curNode.HasChildBy(c))
                 {
-                    throw new TrieNodeException();
+                    return null;
                 }
                 curNode = curNode.NextChildBy(c);
             }
