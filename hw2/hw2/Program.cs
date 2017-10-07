85d5babc855bc4ad5d02dd73ef03100facf5b966
Decorus2009 <Decorus2009@mail.ru>
2017-10-07 22:56:28 +0300

    added hw2

---
+++ hw2/hw2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace hw2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine("Usage:\napplication assembiles_path");
+                /* так можно завершать приложение с кодом ошибки? */
+                Environment.Exit(1);
+            }
+            var path = args[0]; 
+            var testGroups = TestDiscoverer.DiscoverIn(path);
+            TestRunner.Run(testGroups);
+        }
+    }
+}
